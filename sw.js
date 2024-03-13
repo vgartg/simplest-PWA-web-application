3a4fee605f27d6ed0f224bbcf5ddb8e86803ad98
vgartg <gopik539@mail.ru>
2024-03-13 19:37:03 +0300

    Added cache saving and error display when there is no connection

--- sw.js
+++ sw.js
@@ -5,9 +5,9 @@ const staticAssets = [
     './index.html',
     './content/images/icons/icon-128x128.png',
     './content/images/icons/icon-192x192.png',
-    '.style.css',
+    './style.css',
     './content/js/app.js',
-    './content/js/main.js'
+    './content/js/script.js'
 ];
 
 self.addEventListener('install', async event => {
@@ -17,17 +17,26 @@ self.addEventListener('install', async event => {
 });
 
 self.addEventListener('activate', async event => {
-    const cachesKeys = await caches.keys();
-    const checkKeys = cachesKeys.map(async key => {
-        if (![staticCacheName, dynamicCacheName].includes(key)) {
-            await caches.delete(key);
-        }
-    });
-    await Promise.all(checkKeys);
     console.log('Service worker has been activated');
 });
 
 self.addEventListener('fetch', event => {
-    console.log(`Trying to fetch ${event.request.url}`);
-    event.respondWith(checkCache(event.request));
+    event.respondWith(
+        fetch(event.request).catch(() => {
+            return caches.match(event.request).then(cachedResponse => {
+                if (cachedResponse) {
+                    return cachedResponse;
+                }
+                return new Response('Соединение потеряно. Пожалуйста, попробуйте позже.', {
+                    headers: { 'Content-Type': 'text/html' },
+                    status: 503,
+                    statusText: 'Service Unavailable',
+                    body: 'Соединение потеряно. Пожалуйста, попробуйте позже.',
+                    headers: new Headers({
+                        'Content-Type': 'text/html; charset=utf-8'
+                    })
+                });
+            });
+        })
+    );
 });
