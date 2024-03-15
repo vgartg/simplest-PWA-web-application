5f463f086cad16947bc67f921d3e5f1a729220d2
vgartg <gopik539@mail.ru>
2024-03-15 17:56:13 +0300

    Added button for download

--- content/js/app.js
+++ content/js/app.js
@@ -1,13 +1,66 @@
-window.addEventListener('load', () => {
-
-    if ('serviceWorker' in navigator){
-
-        navigator.serviceWorker.register('./sw.js')
-            .then(registration => {
-                console.log('Service worker successfully registered', registration);
-            })
-            .catch(error => {
-                console.log('Service worker registration failed', error);
-            });
+document.addEventListener("DOMContentLoaded", function() {
+    let displayMode = 'browser';
+    const mqStandAlone = '(display-mode: standalone)';
+    if (navigator.standalone || window.matchMedia(mqStandAlone).matches) {
+      displayMode = 'standalone';
+      console.log('standalone there!');
     }
-});
+
+    else {
+        console.log('standalone not there!');
+        downloadModal.style.display = 'block';
+        downloadModalDiv.style.display = 'block';
+    }
+  
+    if (displayMode === 'standalone') {
+      let downloadModal = document.getElementById('downloadModal');
+      downloadModal.style.display = 'none';
+      downloadModalDiv.style.display = 'none';
+    }
+  });
+  
+  window.addEventListener('load', () => {
+  
+      if ('serviceWorker' in navigator){
+  
+          navigator.serviceWorker.register('./sw.js')
+              .then(registration => {
+                  console.log('Service worker successfully registered', registration);
+              })
+              .catch(error => {
+                  console.log('Service worker registration failed', error);
+              });
+      }
+  });
+
+  let deferredPrompt;
+
+  window.addEventListener('beforeinstallprompt', (e) => {
+      // Prevents the default mini-infobar or install dialog from appearing on mobile
+      e.preventDefault();
+      // Save the event because you'll need to trigger it later.
+      deferredPrompt = e;
+      // Call your custom function to show the install promotion UI
+      showInAppInstallPromotion();
+  });
+  
+  // Обработчик события для кнопки установки PWA
+  function showInAppInstallPromotion() {
+    const downloadButton = document.getElementById('downloadButton');
+    downloadButton.addEventListener('click', (e) => {
+      if (deferredPrompt) {
+        // Show the prompt
+        deferredPrompt.prompt();
+        // Wait for the user to respond to the prompt
+        deferredPrompt.userChoice
+          .then((choiceResult) => {
+            if (choiceResult.outcome === 'accepted') {
+              console.log('User accepted the A2HS prompt');
+            } else {
+              console.log('User dismissed the A2HS prompt');
+            }
+            deferredPrompt = null;
+          });
+      }
+    });
+  }
--- sw.js
+++ sw.js
@@ -1,13 +1,14 @@
 const staticCacheName = 'static-cache-v0';
+const dynamicCacheName = 'dynamic-cache-v0';
 
 const staticAssets = [
     './',
     './index.html',
     './content/images/icons/icon-128x128.png',
     './content/images/icons/icon-192x192.png',
-    '.style.css',
+    './style.css',
     './content/js/app.js',
-    './content/js/main.js'
+    './content/js/script.js'
 ];
 
 self.addEventListener('install', async event => {
@@ -31,3 +32,26 @@ self.addEventListener('fetch', event => {
     console.log(`Trying to fetch ${event.request.url}`);
     event.respondWith(checkCache(event.request));
 });
+
+function handleCacheAddError(error) {
+    console.error('Failed to add to cache:', error);
+  }
+
+  async function checkCache(req) {
+    const cachedResponse = await caches.match(req);
+    return cachedResponse || checkOnline(req);
+}
+
+async function checkOnline(req) {
+    const cache = await caches.open(dynamicCacheName);
+    try {
+        const res = await fetch(req);
+        await cache.put(req, res.clone());
+        return res;
+    } catch (error) {
+        const cachedRes = await cache.match(req);
+        if (cachedRes) {
+            return cachedRes;
+        }
+    }
+}
